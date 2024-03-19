46442a8ae7e83ab0b9e8ceb4fd2269824a10e279
Artur Peixoto Lucas <arturpeixoto71@gmail.com>
2024-03-19 15:14:30 -0300

    feat css

--- conexo_wars/urls.py
+++ conexo_wars/urls.py
@@ -17,10 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('admin/', admin.site.urls),
     path('leaderboard/', include('leaderboard.urls', namespace='leaderboard')),
     path('login/', include('login.urls', namespace='login')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
